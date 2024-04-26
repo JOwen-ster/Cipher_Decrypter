a45faf47300f5dbbcd395f7c0bb804bc09298a75
Owen Sterling <jowen.sterling@csu.fullerton.edu>
2024-04-26 16:09:45 -0700

    Update

---
+++ os_p2.py
@@ -0,0 +1,28 @@
+# Import the rsa module used to load the private key file and decrypt binary files
+import rsa
+
+# Function to decrypt the ciphers
+# The function takes in the private key file and the cipher file(s) as arguments
+def decryptciphers(PRIVATE_KEY: str, *args: tuple[str]):
+    # Open and read the private key file as bytes
+    with open(PRIVATE_KEY, "rb") as pk:
+        privatekeydata = rsa.PrivateKey.load_pkcs1(pk.read())
+
+    # Loop through all cipher files and decrypt them one by one
+    for index, filename in enumerate(args, 1):
+        # Open and read the binary cipher file
+        with open(filename, "rb") as cipherfile:
+            # Decrypt the binary cipher file using the private key
+            current_cipher = rsa.decrypt(
+                crypto=cipherfile.read(), priv_key=privatekeydata
+            # Decode the decrypted text to a string
+            ).decode()
+            
+            # Open a new file to write the decrypted text
+            with open(f'os_plaintxt{index}.txt', 'w') as file:
+                # Write the decrypted text to the file
+                file.write(current_cipher)
+
+# Main function to run the decrypter
+if __name__ == "__main__":
+    decryptciphers('prv.key', 'cipher1.bin', 'cipher2.bin')
